92f9254cc1780f9db40b20bced4d67736337a39f
MOHAMMED ASAD KHAN <mdasadkhan67@gmail.com>
2025-04-16 12:32:30 +0530

    Fixed_Offline_and_Online_Data

--- lib/view/splash_screen.dart/splash_screen.dart
+++ lib/view/splash_screen.dart/splash_screen.dart
@@ -13,7 +13,6 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
     Timer(const Duration(seconds: 2), () async {
       Navigator.pushReplacement(
         context,
